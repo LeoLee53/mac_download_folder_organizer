5c6ec7fb707ac778a25202dfaa4afe94c0f8b5c3
Leo Lee <a1442963708@gmail.com>
2024-04-07 12:20:34 +1200

    Redesigned the logic in the file handler class to only work on files and directories added to the downloads folder. Changed to on_created from on_modified to make the logic be triggered more consistently

--- macbook_file_organizer/watch_new_files.py
+++ macbook_file_organizer/watch_new_files.py
@@ -1,4 +1,5 @@
 import concurrent.futures
+import os.path
 import time
 
 from watchdog.events import FileSystemEventHandler
@@ -12,13 +13,21 @@ class FileAddedHandler(FileSystemEventHandler):
         super().__init__()
         self.pool = concurrent.futures.ThreadPoolExecutor(max_workers=5)
 
-    def on_modified(self, event):
-        self.pool.submit(move_files.move_file, event.src_path)
+    # def on_modified(self, event):
+    #     self.pool.submit(move_files.move_file, event.src_path)
+
+    def on_created(self, event):
+        basename = os.path.basename(event.src_path)
+        can_do = ((not event.is_directory) or
+                  not (basename.startswith('*') and basename.endswith('*')))
+
+        if can_do:
+            self.pool.submit(move_files.move_file, event.src_path)
 
 
 observer = Observer()
 handler = FileAddedHandler()
-observer.schedule(handler, path=move_files.DOWNLOAD_FOLDER_PATH, recursive=True)
+observer.schedule(handler, path=move_files.DOWNLOAD_FOLDER_PATH, recursive=False)
 
 observer.start()
 try:
