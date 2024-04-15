2fd76adfaf0fdabbfad13bb19773b7eaf0d39e05
Leo Lee <a1442963708@gmail.com>
2024-04-15 13:26:41 +1200

    improved the logic for handling arc and safari downloads
    improved the logic of detecting how file downloads by using the moved and created events.

--- macbook_file_organizer/watch_new_files.py
+++ macbook_file_organizer/watch_new_files.py
@@ -1,32 +1,39 @@
-import concurrent.futures
 import os.path
 import time
 
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import FileSystemEventHandler, FileSystemEvent
 from watchdog.observers import Observer
 
 import move_files
+from delete_file import _is_category_folder
 
 
-class FileAddedHandler(FileSystemEventHandler):
+class NewDownloadHandler(FileSystemEventHandler):
     def __init__(self):
         super().__init__()
-        self.pool = concurrent.futures.ThreadPoolExecutor(max_workers=5)
 
-    # def on_modified(self, event):
-    #     self.pool.submit(move_files.move_file, event.src_path)
-
-    def on_created(self, event):
+    def on_created(self, event: FileSystemEvent) -> None:
         basename = os.path.basename(event.src_path)
-        can_do = ((not event.is_directory) or
-                  not (basename.startswith('*') and basename.endswith('*')))
+
+        can_do = (not event.is_directory and
+                  not basename.endswith('.crdownload') and
+                  not basename.startswith('.'))
 
         if can_do:
-            self.pool.submit(move_files.move_file, event.src_path)
+            move_files.move_file(event.src_path)
+
+    def on_moved(self, event: FileSystemEvent) -> None:
+        dest_dir_name = os.path.dirname(event.dest_path).split('/')[-1]
+        if _is_category_folder(dest_dir_name):
+            return
+
+        src_dir_name = os.path.dirname(event.src_path).split('/')[-1]
+        if src_dir_name.endswith('.download'):
+            move_files.move_file(event.dest_path)
 
 
 observer = Observer()
-handler = FileAddedHandler()
+handler = NewDownloadHandler()
 observer.schedule(handler, path=move_files.DOWNLOAD_FOLDER_PATH, recursive=False)
 
 observer.start()
